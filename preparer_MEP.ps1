3a8fbe827db53ee3f39e1f094cde89959bc7cb43
PixelLibre <guilbin@hotmail.com>
2024-12-25 13:59:03 -0500

    Update preparer_MEP.ps1

--- preparer_MEP.ps1
+++ preparer_MEP.ps1
@@ -1,1 +1,19 @@
+#Lister les images
 
+#Pour chaque image, vérifier s'il y a un ID pour trouver le maximum
+
+#Si le CSV n'existe pas, le créer.
+#Sinon, lire le CSV comme référence.
+
+#Pour chaque image, inscrire une ID à partir de l'ID maximum + 1
+#Ajouter une ligne dans le CSV
+
+#Lire le modele.html
+
+#Pour chaque ligne du CSV construire l'occurrence de train en HTML
+
+#Remplacer ?occurrences_de_train dans le modèle par les occurrences constuites
+
+#Écrire le résultat dans index.html
+
+read-host "Presser <Entrée> pour teminer"
